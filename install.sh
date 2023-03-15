f369c1085a40fc3c8019405c72345215f75a28f0
shrox42 <127936267+shrox42@users.noreply.github.com>
2023-03-15 15:11:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,3 @@ apt upgrade -y
 apt install cme ffuf jq sqlmap sslscan python3 python3-pip nmap feroxbuster wpscan seclists ncat curl wget python3-impacket gcc golang metasploit-framework ruby-full -y
 
 gem install evil-winrm
-
-mkdir /tmp/test
