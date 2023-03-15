8abee99547af534f325783573d825a3a90d0152d
Ubuntu <azureuser@webserver.jb13k2zq2u5uxolfk5ukzaqjbc.xx.internal.cloudapp.net>
2023-03-15 09:18:40 +0000

    init

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt update 
+apt upgrade -y 
+apt install cme ffuf jq sqlmap sslscan python3 python3-pip nmap feroxbuster wpscan seclists ncat curl wget python3-impacket gcc golang metasploit-framework ruby-full -y
+
+gem install evil-winrm
