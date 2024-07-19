4c84ccf6f7b250c6bccf73f5f2ad3ea9178190f6
Chris <christoph-sprengel@t-online.de>
2024-07-19 11:40:47 +0200

    remove unwanted stuff

--- install.sh
+++ install.sh
@@ -2,16 +2,11 @@
 
 apt update 
 apt upgrade -y 
-apt install cme ffuf jq sqlmap sslscan python3 python3-pip nmap feroxbuster wpscan seclists ncat curl wget python3-impacket gcc golang metasploit-framework ruby-full whatweb spice-vdagent qemu-guest-agent -y
+apt install ffuf jq sqlmap sslscan python3 python3-pip nmap feroxbuster wpscan seclists ncat curl wget python3-impacket gcc golang metasploit-framework ruby-full whatweb  -y
 
 gem install evil-winrm
 
 
-### install auto resize script
-mkdir /opt/autoresize
-cp ./autoresize.sh /opt/autoresize/
-chmod +x /opt/autoresize/autoresize.sh
-sudo ln -s /opt/autoresize/autoresize.sh /usr/local/bin/autoresize
 
 
 
