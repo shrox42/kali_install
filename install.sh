2d857ebef925ebc23aef38b03d52ff6dd9f36336
Chris <christoph-sprengel@t-online.de>
2024-07-19 11:48:11 +0200

    install netexec

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 apt update 
 apt upgrade -y 
-apt install ffuf jq sqlmap sslscan python3 python3-pip nmap feroxbuster wpscan seclists ncat curl wget python3-impacket gcc golang metasploit-framework ruby-full whatweb  -y
+apt install pipx git ffuf jq sqlmap sslscan python3 python3-pip nmap feroxbuster wpscan seclists ncat curl wget python3-impacket gcc golang metasploit-framework ruby-full whatweb  -y
 
 gem install evil-winrm
 
+pipx ensurepath
+
+pipx install git+https://github.com/Pennyw0rth/NetExec
 
 
 
-
-#### install burp
-bash ./DownloadAndINstallBurp.sh
